fad03c7eec7950dbc4addf4347cc5ab352abe616
Madhurima <madhurimaofficial7>
2025-03-02 11:58:04 +0530

    first commit

---
+++ src/main/java/in/ashokit/o1_springbt_git/O1SpringbtGitApplication.java
@@ -0,0 +1,13 @@
+package in.ashokit.o1_springbt_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class O1SpringbtGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(O1SpringbtGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/ashokit/o1_springbt_git/O1SpringbtGitApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ashokit.o1_springbt_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class O1SpringbtGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
